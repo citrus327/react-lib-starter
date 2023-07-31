f8908f078d766d6628f0b3d8ab261f151d7a7cda
citrus327 <citrus327@outlook.com>
2023-07-31 14:35:02 +0800

    feat: panda integration

--- src/Box/index.tsx
+++ src/Box/index.tsx
@@ -1,7 +1,36 @@
 import type { BoxProps } from "../Box/types";
 import * as React from "react";
 import { css } from "../../styled-system/css";
+import { styled, Grid } from "../../styled-system/jsx";
+
+const styles = (isBorderless: boolean) =>
+  css({
+    backgroundColor: "gainsboro",
+    borderRadius: isBorderless ? "0px" : "9999px",
+    fontSize: "13px",
+    padding: "10px 15px",
+    _before: { content: '"👋"' },
+    _hover: {
+      color: "red",
+    },
+  });
 
 export const Box: React.FC<BoxProps> = () => {
-  return <div className={css({ bg: "red.400" })}>Hello</div>;
+  const [isBorderless, setIsBorderless] = React.useState(false);
+
+  return (
+    <Grid gridTemplateColumns={2}>
+      <styled.button
+        bg="blue.500"
+        cursor="pointer"
+        onClick={() => {
+          setIsBorderless(!isBorderless);
+        }}
+      >
+        StyledButton
+      </styled.button>
+
+      <div className={styles(isBorderless)}>FRAME</div>
+    </Grid>
+  );
 };
